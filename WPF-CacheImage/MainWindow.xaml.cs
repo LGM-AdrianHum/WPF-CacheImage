a580ac28df4854c5a750a7c21350594bef940116
admin <adrian.hum@yvw.com.au>
2015-10-08 12:16:34 +1100

    Quick cleanup of code... New mainwindow code, remove old redundant code.

--- WPF-CacheImage/MainWindow.xaml.cs
+++ WPF-CacheImage/MainWindow.xaml.cs
@@ -2,15 +2,11 @@
 // Project: WPF-CacheImage/WPF-CacheImage/MainWindow.xaml.cs
 //
 // Created  Date: 2015-10-08  11:05 AM
-// Modified Date: 2015-10-08  11:33 AM
+// Modified Date: 2015-10-08  12:15 PM
 
 #region Using Directives
 
-using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows;
-using WPF_CacheImage.Annotations;
 
 #endregion
 
@@ -19,34 +15,18 @@ namespace WPF_CacheImage
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : INotifyPropertyChanged
+    public partial class MainWindow
     {
-        private string _url;
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
 
         private void GetUrlButton_OnClick(object sender, RoutedEventArgs e)
         {
             var dc = DataContext as MyWorkingViewModel;
             if (dc == null) return;
             dc.SetUrlIfValid();
-        }
-
-        /// <exception cref="Exception">A delegate callback throws an exception.</exception>
-        [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            var handler = PropertyChanged;
-            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
